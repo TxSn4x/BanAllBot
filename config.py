bddbb60eb6ade0d65c7f940ff08d49a4f6ffc2f1
TxSn4x <txsn4x@gmail.com>
2026-01-18 23:31:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 class Config:
-    API_ID="26657288"
-    API_HASH="00536e431477dbb16583d5b85813aa72"
-    TOKEN="8322614110:AAH3BjISKBM1YGHoUdJpeSy4oNp7Skvb4UI"
+    API_ID=24509589
+    API_HASH="717cf21d94c4934bcbe1eaa1ad86ae75"
+    TOKEN="8084144216:AAEKmJkS-jwFFlmAtwvJaCoNQlzv1s5FJkQ"
     SUDO = list(int(i) for i in os.environ.get("SUDO", "8158464263").split(" "))
-    START_IMG="https://graph.org/file/6d30f418db49172de15dc-7abbc00b7db69788f9.jpg"
-    BOT_ID=8322614110
-    BOT_USERNAME="BanAllRbot"
-    BOT_NAME="banall"
+    START_IMG="https://telegra.ph/file/52fefb8bd51289a83a49b.jpg"
+    BOT_ID=8084144216
+    BOT_USERNAME="ZuliaiBot"
+    BOT_NAME="ZuliaiBot"
